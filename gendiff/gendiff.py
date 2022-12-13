0e8d81b4edecd3407e898e1c4ceb48d587e4dc0b
Olga Solodko <jasminess847@gmail.com>
2022-12-13 21:18:55 +0300

    change algorithm for gendiff

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -6,47 +6,36 @@ def generate_diff(filepath1, filepath2):
     file1 = json.load(open(filepath1))
     file2 = json.load(open(filepath2))
     all_keys = set(file1) | set(file2)
+    difference = set(file1) - set(file2)
+    added = set(file2) - set(file1)
+    general = set(file1) & set(file2)
     indent = '    '
     indent_neg = '  - '
     indent_pos = '  + '
 
-    diff = get_diff(all_keys, file1, file2)
     lines = []
-    for elem in diff:
-        for key, value in elem.items():
-            k, v = value
-            v = transform_bool(v)
-            if key == 'removed':
-                line = create_line(indent_neg, k, v)
-
-            elif key == 'added':
-                line = create_line(indent_pos, k, v)
-
-            elif key == 'unchanged':
-                line = create_line(indent, k, v)
-            elif key == 'changed -':
-                line = create_line(indent_neg, k, v)
+    print(general)
+    print(difference)
+    print(added)
+    for elem in sorted(all_keys):
+        if elem not in difference and elem in general:
+            if file1.get(elem) == file2.get(elem):
+                line = create_line(indent, elem, file2.get(elem))
+                lines.append(line)
             else:
-                line = create_line(indent_pos, k, v)
+                line1 = create_line(indent_neg, elem, file1.get(elem))
+                line2 = create_line(indent_pos, elem, file2.get(elem))
+                lines.append(line1)
+                lines.append(line2)
+        elif elem not in added:
+            line = create_line(indent_neg, elem, file1.get(elem))
+            lines.append(line)
+        else:
+            line = create_line(indent_pos, elem, file2.get(elem))
 
-        lines.append(line)
+            lines.append(line)
     result = chain('{', lines, '}')
     return '\n'.join(result)
-
-
-def get_diff(all_keys, file1, file2):
-    diff = []
-    for key in sorted(all_keys):
-        if key not in file1:
-            diff.append({'added': (key, file2.get(key))})
-        elif key not in file2:
-            diff.append({'removed': (key, file1.get(key))})
-        elif file1.get(key) == file2.get(key):
-            diff.append({'unchanged': (key, file1.get(key))})
-        else:
-            diff.append({'changed -': (key, file1.get(key))})
-            diff.append({'changed +': (key, file2.get(key))})
-    return diff
 
 
 def transform_bool(value):
@@ -56,4 +45,5 @@ def transform_bool(value):
 
 
 def create_line(indent, key, value):
+    value = transform_bool(value)
     return f'{indent}{key}: {value}'
