1dcf77b27c75e4caa92d27b2a8b52ef34cf24465
olga <jasminess847@gmail.com>
2023-02-26 19:27:41 +0300

    update extending keys

--- gendiff/comparator2.py
+++ gendiff/comparator2.py
@@ -3,24 +3,24 @@ import json
 
 def generate_diff(path1, path2):
     dict1, dict2 = parse_data(path1, path2)
-    def iter_(dict1, dict2, lines={}):
-        # lines = {}
+    def iter_(dict1, dict2):
+        lines = {}
         for key in dict1:
             if key in dict2:
                 if isinstance(dict1.get(key), dict) and isinstance(dict2.get(key), dict):
-                    lines[key] = f'{iter_(dict1.get(key), dict2.get(key))}'
-                if dict1.get(key) == dict2.get(key):
-                    lines['unchanged'] = f' {key}: {dict2.get(key)}'
+                    lines[key] = (f'{iter_(dict1.get(key), dict2.get(key))}')
+                elif dict1.get(key) == dict2.get(key):
+                    lines['unchanged']=lines.get('unchanged', []) + [f' {key}: {dict2.get(key)}']
                 else:
-                    lines['changed'] = [
+                    lines['changed']=lines.get('changed', []) + [
                         f'- {key}: {dict1.get(key)}',
                         f'+ {key}: {dict2.get(key)}'
                     ]
             elif key not in dict2:
-                lines['removed']= f'- {key}: {dict1.get(key)}'
+                lines['removed']= lines.get('removed', []) + [f'- {key}: {dict1.get(key)}']
         for key in dict2:
             if key not in dict1:
-                lines['added'] = f'+ {key}: {dict2.get(key)}'
+                lines['added']= lines.get('added', []) + [(f'+ {key}: {dict2.get(key)}')]
         return lines
     return iter_(dict1, dict2)
 
