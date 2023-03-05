b2dd7371ae31401cd0f8f43a49bf80c6a3565a84
olga <jasminess847@gmail.com>
2023-03-05 21:13:58 +0300

    make stylish without digging

--- gendiff/stylish.py
+++ gendiff/stylish.py
@@ -2,7 +2,7 @@ from itertools import chain
 
 def formatter(dicti, space_counts=' ', counter=4):
     def iter_(current_value, depth):
-        if not isinstance(current_value, list):
+        if not isinstance(current_value, dict) and not isinstance(current_value, list):
             return str(current_value)
         indent_size = depth + counter  # размер отступа текущего уровня вложенности
         actual_indent = space_counts * (indent_size-2)  # вычисление отступа по формуле 
@@ -10,20 +10,21 @@ def formatter(dicti, space_counts=' ', counter=4):
         lines = []
         for elem in current_value:
             for key, val in elem.items():
-                if key == 'added':
-                    lines.append(f'{actual_indent}+ {iter_(current_value[elem
-                                 ], indent_size)}')
-                elif] elem == 'removed':
-                    lines.append(f'{actual_indent}- {iter_(current_value[elem], indent_size)}')
-                elif elem == 'changed':
-                    lines.append(f'{actual_indent}- {iter_(current_value[elem], indent_size)}')
-                    
+                if key == 'removed':
+                    lines.append(f'{actual_indent}- {val}')
+                elif key == 'added':
+                    lines.append(f'{actual_indent}+ {val}')
+                elif key == 'changed':
+                    for k, v in val.items():
+                        lines.append(f'{actual_indent}- {k}: {v[0]}')
+                        lines.append(f'{actual_indent}+ {k}: {v[1]}')
                 else:
-                    lines.append(f'{actual_indent}  {elem}: {iter_(current_value[elem], indent_size)}')
+                    lines.append(f'{actual_indent}  {key}: {iter_(val, indent_size)}')
         result = chain('{', lines, [previous_indent +'}'])
         return '\n'.join(result)
     return iter_(dicti, depth=0)
 
 d = [{'common': [{'added': {'follow': False}}, {'setting1': 'Value 1'}, {'removed': {'setting2': 200}}, {'changed': {'setting3': (None, True)}}, {'added': {'setting4': 'blah blah'}}, {'added': {'setting5': {'key5': 'value5'}}}, {'setting6': [{'doge': [{'changed': {'wow': ('so much', '')}}]}, {'key': 'value'}, {'added': {'ops': 'vops'}}]}]}, {'group1': [{'changed': {'baz': ('bars', 'bas')}}, {'foo': 'bar'}, {'changed': {'nest': ('str', {'key': 'value'})}}]}, {'removed': {'group2': {'abc': 12345, 'deep': {'id': 45}}}}, {'added': {'group3': {'deep': {'id': {'number': 45}}, 'fee': 100500}}}]
 print(formatter(d))
+#formatter(d)
 
