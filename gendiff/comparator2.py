3fd96c355f6d1cdb00026e9bf0a98dbe7647ded8
volga <volga@volga.fritz.box>
2023-03-10 00:18:33 +0300

    modified comparator & formatter

--- gendiff/comparator2.py
+++ gendiff/comparator2.py
@@ -1,27 +1,23 @@
 from gendiff.parser import parse_data
 import json
-#from gendiff.stylish import formatter
-
-
 def generate_diff(path1, path2):
     dict1, dict2 = parse_data(path1, path2)
     def iter_(dict1, dict2):
-        diff = []
+        diff = {}
         for key in sorted(set(dict1.keys()) | set(dict2.keys())):
             if key in dict1 and key not in dict2:
-                diff.append({'added': {key: dict1[key]}})
+                diff[key] = ('removed', dict1[key])
             elif key in dict2 and key not in dict1:
-                diff.append({'removed': {key: dict2[key]}})
+                diff[key] = ('added', dict2[key])
             elif dict1[key] != dict2[key]:
                 if isinstance(dict1[key], dict) and isinstance(dict2[key], dict):
-                    diff.append(({key: iter_(dict1[key], dict2[key])}))
+                    diff[key] = iter_(dict1[key],dict2[key])
                 else:
-                    diff.append({'changed': {key: (dict1.get(key),dict2.get(key))}})
+                    diff[key] = ('changed', dict1[key], dict2[key])
             else:
-                diff.append({key: dict1[key]})
+                diff[key] = ('unchanged', dict1[key])
         return diff
     return iter_(dict1, dict2)
-
-print(generate_diff('tests/fixtures/plain1.json', 'tests/fixtures/plain2.json'))
+print(generate_diff('tests/fixtures/nested1.json', 'tests/fixtures/nested2.json'))
 
 #formatter(generate_diff('tests/fixtures/nested1.json', 'tests/fixtures/nested2.json'))
--- gendiff/stylish.py
+++ gendiff/stylish.py
@@ -1,30 +1,62 @@
 from itertools import chain
 
-def formatter(dicti, space_counts=' ', counter=4):
-    def iter_(current_value, depth):
-        if not isinstance(current_value, dict) and not isinstance(current_value, list):
-            return str(current_value)
-        indent_size = depth + counter  # размер отступа текущего уровня вложенности
-        actual_indent = space_counts * (indent_size-2)  # вычисление отступа по формуле 
-        previous_indent = space_counts * depth  # это размер предыдущего уровня вложенности
+# функция которая будет раскрывать нераскрытые словари
+def stringify_value(value, space_counts = ' ', counter=4):
+    def iter_(cur_value, depth):
+        if not isinstance(cur_value, dict):
+            return change_value(cur_value)
         lines = []
-        for elem in current_value:
-            for key, val in elem.items():
-                if key == 'removed':
-                    lines.append(f'{actual_indent}- {val}')
-                elif key == 'added':
-                    lines.append(f'{actual_indent}+ {val}')
-                elif key == 'changed':
-                    for k, v in val.items():
-                        lines.append(f'{actual_indent}- {k}: {v[0]}')
-                        lines.append(f'{actual_indent}+ {k}: {v[1]}')
-                else:
-                    lines.append(f'{actual_indent}  {key}: {iter_(val, indent_size)}')
+        indent_size = counter + depth
+        actual_indent = space_counts * (indent_size-2)
+        current_indent = space_counts * depth
+        for key, val in cur_value.items():
+            
+            lines.append(f'{actual_indent}  {key}: {iter_(val, indent_size)}')
+            
+        result = chain('{', lines, [current_indent +'}'])
+        return '\n'.join(result)
+    return iter_(value, depth=4)
+
+
+def formatter(value, space_counts=' ', counter=4):
+    def iter_(current_value, depth):
+        if not isinstance(current_value, dict):
+            return change_value(current_value)
+        lines = []
+        indent_size = depth + counter
+        actual_indent = space_counts * (indent_size-2)
+        previous_indent = space_counts * depth
+        for key, val in current_value.items():
+            if isinstance(val, tuple):
+                if val[0] == 'added':
+                    lines.append(f'{actual_indent}+ {key}: {stringify_value(val[1])}')
+                elif val[0] == 'removed':
+                    lines.append(f'{actual_indent}- {key}: {stringify_value(val[1])}')
+                elif val[0] == 'changed':
+                    lines.append(f'{actual_indent}- {key}: {stringify_value(val[1])}')
+                    lines.append(f'{actual_indent}+ {key}: {stringify_value(val[2])}')
+                elif val[0] == 'unchanged':
+                    lines.append(f'{actual_indent}  {key}: {stringify_value(val[1])}')
+            else:
+                lines.append(f'{actual_indent}  {key}: {stringify_value(iter_(val, indent_size))}')
         result = chain('{', lines, [previous_indent +'}'])
         return '\n'.join(result)
-    return iter_(dicti, depth=0)
+    return iter_(value, 0)
 
-d = [{'common': [{'added': {'follow': False}}, {'setting1': 'Value 1'}, {'removed': {'setting2': 200}}, {'changed': {'setting3': (None, True)}}, {'added': {'setting4': 'blah blah'}}, {'added': {'setting5': {'key5': 'value5'}}}, {'setting6': [{'doge': [{'changed': {'wow': ('so much', '')}}]}, {'key': 'value'}, {'added': {'ops': 'vops'}}]}]}, {'group1': [{'changed': {'baz': ('bars', 'bas')}}, {'foo': 'bar'}, {'changed': {'nest': ('str', {'key': 'value'})}}]}, {'removed': {'group2': {'abc': 12345, 'deep': {'id': 45}}}}, {'added': {'group3': {'deep': {'id': {'number': 45}}, 'fee': 100500}}}]
-print(formatter(d))
+def change_value(value):
+    if isinstance(value, (str, bool, int)):
+        return str(value).lower()
+    elif value == None:
+        return 'null'
+
+# s = {'nest': {
+#         'key': 'value'
+#     }}
+# l = [{'common': [{'removed': {'follow': False}}, {'setting1': 'Value 1'}, {'added': {'setting2': 200}}, {'changed': {'setting3': (None, True)}}, {'removed': {'setting4': 'blah blah'}}, {'removed': {'setting5': {'key5': 'value5'}}}, {'setting6': [{'doge': [{'changed': {'wow': ('so much', '')}}]}, {'key': 'value'}, {'removed': {'ops': 'vops'}}]}]}, {'group1': [{'changed': {'baz': ('bars', 'bas')}}, {'foo': 'bar'}, {'changed': {'nest': ('str', {'key': 'value'})}}]}, {'added': {'group2': {'abc': 12345, 'deep': {'id': 45}}}}, {'removed': {'group3': {'deep': {'id': {'number': 45}}, 'fee': 100500}}}]
+# # d = {'common': {'follow': ('added', False), 'setting1': 'Value 1', 'setting2': ('removed', 200), 'setting3': ('changed', True, None), 'setting4': ('added', 'blah blah'), 'setting5': ('added', {'key5': 'value5'}), 'setting6': {'doge': {'wow': ('changed', '', 'so much')}, 'key': 'value', 'ops': ('added', 'vops')}}, 'group1': {'baz': ('changed', 'bas', 'bars'), 'foo': 'bar', 'nest': ('changed', {'key': 'value'}, 'str')}, 'group2': ('removed', {'abc': 12345, 'deep': {'id': 45}}), 'group3': ('added', {'deep': {'id': {'number': 45}}, 'fee': 100500})}
+# plain = [{'removed': {'follow': False}}, {'host': 'hexlet.io'}, {'removed': {'proxy': '123.234.53.22'}}, {'changed': {'timeout': (50, 20)}}, {'added': {'verbose': True}}]
+# d = {'common': {'follow': ('added', False), 'setting1': ('unchanged', 'Value 1'), 'setting2': ('removed', 200), 'setting3': ('changed', True, None), 'setting4': ('added', 'blah blah'), 'setting5': ('added', {'key5': 'value5'}), 'setting6': {'doge': {'wow': ('changed', '', 'so much')}, 'key': ('unchanged', 'value'), 'ops': ('added', 'vops')}}, 'group1': {'baz': ('changed', 'bas', 'bars'), 'foo': ('unchanged', 'bar'), 'nest': ('changed', {'key': 'value'}, 'str')}, 'group2': ('removed', {'abc': 12345, 'deep': {'id': 45}}), 'group3': ('added', {'deep': {'id': {'number': 45}}, 'fee': 100500})}
+# print(formatter(d))
 #formatter(d)
+
 
