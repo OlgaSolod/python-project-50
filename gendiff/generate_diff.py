6a39a320e402ae8163e113e11cc87d9c2ad4f512
Olga <jasminess847@gmail.com>
2023-03-25 20:39:28 +0300

    del debug data

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -53,8 +53,3 @@ def is_changed(key, dict1, dict2):
 
 def is_nested(key, dict1, dict2):
     return is_dict(dict1[key]) and is_dict(dict2[key])
-
-# dict1, dict2 = reading_files('tests/fixtures/nested1.json', 'tests/fixtures/nested2.json')
-# dict1 = {'common': {'setting1': 'Value 1', 'setting2': 200, 'setting3': True, 'setting6': {'key': 'value', 'doge': {'wow': ''}}}, 'group1': {'baz': 'bas', 'foo': 'bar', 'nest': {'key': 'value'}}, 'group2': {'abc': 12345, 'deep': {'id': 45}}}
-# dict2 = {'common': {'follow': False, 'setting1': 'Value 1', 'setting3': None, 'setting4': 'blah blah', 'setting5': {'key5': 'value5'}, 'setting6': {'key': 'value', 'ops': 'vops', 'doge': {'wow': 'so much'}}}, 'group1': {'foo': 'bar', 'baz': 'bars', 'nest': 'str'}, 'group3': {'deep': {'id': {'number': 45}}, 'fee': 100500}}
-# print(build_tree(dict1, dict2))
